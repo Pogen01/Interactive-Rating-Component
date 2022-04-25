2f77a0b4b673cddf2c13a368d4288d84d48d99b9
Pogen <76914800+IPogenI@users.noreply.github.com>
2022-04-26 04:46:11 +0600

    final draft v2

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-// function user_input_1(){
-//     user_rating = document.getElementById("ratings_1").innerHTML
-//     document.getElementById("user-rating").innerHTML = user_rating
-// }
-
-// function user_input_2(){
-//     ratings_2.classList.toggle("active");
-//     user_rating = document.getElementById("ratings_2").innerHTML
-//     document.getElementById("user-rating").innerHTML = user_rating
-// }
-
-// function user_input_3(){
-//     user_rating = document.getElementById("ratings_3").innerHTML
-//     document.getElementById("user-rating").innerHTML = user_rating
-// }
-
-// function user_input_4(){
-//     user_rating = document.getElementById("ratings_4").innerHTML
-//     document.getElementById("user-rating").innerHTML = user_rating
-// }
-
-// function user_input_5(){
-//     user_rating = document.getElementById("ratings_5").innerHTML
-//     document.getElementById("user-rating").innerHTML = user_rating
-// }
-//Old Process Above - thoughts?, thought I might come back to this to see how much I learned!
-
-
 function submit_button(){
     document.getElementById("hide").classList.toggle("hide")
     document.getElementById("open").classList.toggle("hide")
